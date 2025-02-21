ed4dd36470ac5c96c9af7eecc54140eeec3d414f
Medaillek <medaillek@gmail.com>
2025-02-21 17:35:51 +0100

    feat: update indexTemplate and makeActionDir to include featureName in action file exports

--- src/templates/server/actions/index.ts
+++ src/templates/server/actions/index.ts
@@ -2,14 +2,14 @@ import { createActionTemplate as cat } from './create'
 import { deleteActionTemplate as dat } from './delete'
 import { updateActionTemplate as uat } from './update'
 
-import { DataError, makeFile } from '../../../utils'
+import { capitalize, DataError, makeFile } from '../../../utils'
 import { PermissionType } from '../../../data'
 
-function indexTemplate() {
+function indexTemplate(featureName: string): string {
 	return `
-    export * from './create'
-    export * from './delete'
-    export * from './update'
+    export * from './create${featureName}Action'
+    export * from './delete${featureName}Action'
+    export * from './update${featureName}Action'
    `.trim()
 }
 
@@ -18,14 +18,15 @@ export function makeActionDir(
 	permissionType: PermissionType,
 	path: string
 ): Promise<DataError<string>[]> {
+	const capitalized = capitalize(featureName)
 	const createActionTemplate = cat(featureName, permissionType)
 	const deleteActionTemplate = dat(featureName, permissionType)
 	const updateActionTemplate = uat(featureName, permissionType)
-	const indexFile = indexTemplate()
+	const indexFile = indexTemplate(capitalized)
 
-	const createPath = `${path}/create.ts`
-	const deletePath = `${path}/delete.ts`
-	const updatePath = `${path}/update.ts`
+	const createPath = `${path}/create${capitalized}Action.ts`
+	const deletePath = `${path}/delete${capitalized}Action.ts`
+	const updatePath = `${path}/update${capitalized}Action.ts`
 	const indexPath = `${path}/index.ts`
 
 	const createFileProm = makeFile(createPath, createActionTemplate)
